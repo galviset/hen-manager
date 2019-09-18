be01d28438ae26cf9b60d7fe610b5c044d96d901
Alviset <guillaume.alviset@live.fr>
2019-09-18 13:35:24 +0200

    Centralized configuration and enable sunrise/sunset hatch control

--- temperature_log.py
+++ temperature_log.py
@@ -22,7 +22,7 @@ def get_openweather_cond(city_id, api_key):
     return ext_data
 
 
-def fan_control_tresholds(high_temp, high_hum, low_hum, temp, hum, ext_hum, fan_file, temp_priority=True):
+def fan_control_tresholds(high_temp, high_hum, low_hum, temp, hum, ext_hum, fan_file, low_temp, temp_priority=True):
     with open(fan_file, 'w') as fanstate:
         if temp_priority:
             if temp > high_temp:
@@ -32,6 +32,8 @@ def fan_control_tresholds(high_temp, high_hum, low_hum, temp, hum, ext_hum, fan_
             elif ext_hum > high_hum:
                 fanstate.write("off")
             elif hum < low_hum:
+                fanstate.write("off")
+            elif temp < low_temp:
                 fanstate.write("off")
 
 
@@ -44,20 +46,32 @@ if __name__ == '__main__':
     Call this script in capture mode frequently in the day then call it in draw mode at 00:00 AM.
     """
     #daemonizer.DaemonKiller.handle()
+    script_file = os.path.realpath(__file__)
+    script_dir = script_file.split('/')
+    wd ='/'
+    for i in range(1, len(script_dir)-1):
+        wd += script_dir[i]+'/'
+    os.chdir(wd)
 
-    config = configparser.RawConfigParser()
+    config = configparser.ConfigParser()
     config.read("conf.cfg")
-    ow_city = config.getstr('OpenWeather', 'City_ID')
-    ow_key = config.getstr('OpenWeather', 'API_key')
-    tick = config.getint('OpenWeather', 'frequency')
-    fan = config.getstr('Main', 'fan_file')
-    values = config.getstr('Main', 'values_t_h')
+    ow_city = config.get('openweather', 'City_ID')
+    ow_key = config.get('openweather', 'API_key')
+    tick = config.getint('openweather', 'frequency')
+    fan = config.get('main', 'fan_file')
+    values = config.get('main', 'values_t_h')
+    #Fan values
+    temp_act = config.get('fan', 'temp_act')
+    temp_deact = config.get('fan', 'temp_deact')
+    hum_act = config.get('fan', 'hum_act')
+    hum_deact = config.get('fan', 'hum_deact')
+
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('values', type=str, help="File path where the values are stored")
-    parser.add_argument('tick', type=int, help="Time between each value collection.")
-    parser.add_argument('-f', '--fan', type=str, help="Path to the file containing fan state")
-    args = parser.parse_args()
+    #parser.add_argument('values', type=str, help="File path where the values are stored")
+    #parser.add_argument('tick', type=int, help="Time between each value collection.")
+    #parser.add_argument('-f', '--fan', type=str, help="Path to the file containing fan state")
+    #args = parser.parse_args()
 
     print("Setting up DHT sensor...")
     GPIO.setmode(GPIO.BOARD)
@@ -79,13 +93,14 @@ if __name__ == '__main__':
             ext_d = {"temp":0, "hum":0}
         if fan is None:
             fan = os.getenv("HOME") + "/fanstate"
-        fan_control_tresholds(high_temp=28.0,
-                              high_hum=70.0,
-                              low_hum=60.0,
+        fan_control_tresholds(high_temp=temp_act,
+                              high_hum=hum_act,
+                              low_hum=hum_deact,
                               temp=data[1],
                               hum=data[0],
                               ext_hum=float(ext_d['hum'])+10.0,
-                              fan_file=fan
+                              fan_file=fan,
+                              low_temp=temp_act
                               )
         # # Fan control
         # if data[1] > 30.0 or data[0] > 70.0:
